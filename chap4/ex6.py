9400ac0b82b74f2caf462ac12919d23e73f5d200
namber0 <namnb2402@gmail.com>
2023-03-24 10:53:50 +0700

    added input feature to ex6

--- chap4/ex6.py
+++ chap4/ex6.py
@@ -2,4 +2,8 @@ def midpoint(x1, x2, y1, y2):
     print((x1 + y1) / 2)
     print((x2 + y2) / 2)
 
-midpoint(0, 5, 10, 20)
+x1 = int(input("enter number for x1:"))
+x2 = int(input("enter number for x2:"))
+y1 = int(input("enter number for y1:"))
+y2 = int(input("enter number for y2:"))
+midpoint(x1, x2, y1, y2)
