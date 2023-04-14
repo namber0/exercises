32239bdf024b7cc5c7d4743b719cfc744cd02e41
namber0 <namnb2402@gmail.com>
2023-04-14 07:25:42 +0700

    modified ex5 and ex7 of chap7

--- chap7/ex5.py
+++ chap7/ex5.py
@@ -1,15 +1,19 @@
 def factorial(num):
     if num == 1 or num == 0:
-        return 1
+        return 1;
     
-    return num * factorial(num - 1)
+    return num * factorial(num - 1);
 
 def fact(num):
     out = 1
     while num >= 1:
-        out *= num
-        num -= 1
-    return out
+        out *= num;
+        num -= 1;
+    return out;
 
-print(factorial(10))
-print(fact(10))
+def binomial(n, k):
+    return fact(n) / (fact(k) * fact(n - k));
+
+# print(factorial(10));
+print(fact(10));
+print(binomial(10, 2));
--- chap7/ex7.py
+++ chap7/ex7.py
@@ -1,8 +1,33 @@
-num = input('enter 3 numbers: ')
+def maximum(collection):
+    maxNum = collection[0];
+    for i in range(len(collection)):
+        if collection[i] > maxNum:
+            maxNum = collection[i];
+    return maxNum;
 
-num = num.split(' ')
-num = [int(i) for i in num]
+def minimum(collection):
+    minNum = collection[0]
+    for i in range(len(collection)):
+        if collection[i] < minNum:
+            minNum = collection[i];
+    return minNum;
 
-print('largest:', str(max(num)))
-print('smallest:', str(min(num)))
-print('average: %.2f' % (sum(num) / len(num)))
+def sumarize(collection):
+    total = 0;
+    for num in collection:
+        total += num;
+    return total;
+
+num = input('enter 3 numbers: ');
+
+num = num.split(' ');
+
+# for nums in num:
+#     if nums == '':
+#         num.remove(nums);
+
+num = [int(i) for i in num];
+
+print('largest:', str(maximum(num)));
+print('smallest:', str(minimum(num)));
+print('average: %.2f' % (sumarize(num) / len(num)));
