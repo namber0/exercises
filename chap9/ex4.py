deecd19d3f5ff4e8669cd03b4e71314cdb8a14ce
namber0 <namnb2402@gmail.com>
2023-04-28 10:08:43 +0700

    modified ex4 in chap9

--- chap9/ex4.py
+++ chap9/ex4.py
@@ -2,16 +2,16 @@ import re
 
 def findCommonWords(file1, file2, file3):
     with open(file1, 'r') as f1, open(file2, 'r') as f2, open(file3, 'r') as f3:
-        text1 = f1.read()
-        text2 = f2.read()
-        text3 = f3.read()
+        text1 = f1.read();
+        text2 = f2.read();
+        text3 = f3.read();
 
-    words1 = set(re.findall(r'\b\w{10,}\b', text1.lower()))
-    words2 = set(re.findall(r'\b\w{10,}\b', text2.lower()))
-    words3 = set(re.findall(r'\b\w{10,}\b', text3.lower()))
+    words1 = set(re.findall(r'\b\w{10,}\b', text1.lower()));
+    words2 = set(re.findall(r'\b\w{10,}\b', text2.lower()));
+    words3 = set(re.findall(r'\b\w{10,}\b', text3.lower()));
 
-    commonWords = words1 & words2 & words3
-    return commonWords
+    commonWords = words1 & words2 & words3;
+    return commonWords;
 
 commonWords = findCommonWords('file1.txt', 'file2.txt', 'file3.txt');
 print(commonWords);
